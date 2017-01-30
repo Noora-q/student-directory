dbbfaf89a366de89eda88d95b4ff53655fa04d00
Noora <n.k.alqasimi@gmail.com>
2017-01-30 22:20:00 +0000

    Format output to center

--- directory.rb
+++ directory.rb
@@ -1,19 +1,19 @@
 
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(60)
+  puts "To finish, just hit return twice".center(60)
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
-    puts "Please enter the student's country"
+    puts "Please enter the student's country".center(60)
     country = gets.chomp
     # add the student hash to the array
     students << {name: name, cohort: :november, country: country, language: :ruby}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students".center(60)
     # get another name from the user
     name = gets.chomp
   end
@@ -22,18 +22,18 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(60)
+  puts "-------------".center(60)
 end
 
 
 def print(students)
-  puts "Please choose an initial"
+  puts "Please choose an initial".center(60)
   # gets initial
   initial = gets.chomp.upcase
   students.each_with_index do |student, index|
     while student[:name].start_with?(initial) && student[:name].length < 12
-        puts "#{index + 1}. #{student[:name]} from #{student[:country]} learning #{student[:language]} (#{student[:cohort]} cohort)"
+        puts "#{index + 1}. #{student[:name]} from #{student[:country]} learning #{student[:language]} (#{student[:cohort]} cohort)".center(60)
         break
     end
   end
@@ -41,7 +41,7 @@ end
 
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(60)
 end
 
 students = input_students
