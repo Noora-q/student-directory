097432631730e1f9801152a95e41eacd007459e3
Noora <n.k.alqasimi@gmail.com>
2017-01-30 18:59:57 +0000

    Rewrite code to use while loop

--- directory.rb
+++ directory.rb
@@ -24,16 +24,29 @@ def print_header
   puts "-------------"
 end
 
+# def print(students)
+#   puts "Please choose an initial"
+#   # gets initial
+#   initial = gets.chomp.upcase
+#   students.each_with_index do |student, index|
+#     if student[:name].start_with?(initial) && student[:name].length < 12
+#     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+#     end
+#   end
+# end
+
 def print(students)
   puts "Please choose an initial"
   # gets initial
   initial = gets.chomp.upcase
   students.each_with_index do |student, index|
-    if student[:name].start_with?(initial) && student[:name].length < 12
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    while student[:name].start_with?(initial) && student[:name].length < 12
+        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+        break
     end
   end
 end
+
 
 
 def print_footer(names)
