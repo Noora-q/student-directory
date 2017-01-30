62cdf01e0f9c3e82ed94273bccdfaf95ecd87e6e
Noora <n.k.alqasimi@gmail.com>
2017-01-30 17:56:45 +0000

    Get names beginning with specific letter

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -23,10 +25,16 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  puts "Please choose an initial"
+  # gets initial
+  initial = gets.chomp
+  students.each_with_index do |student, index|
+    if student[:name].start_with?(initial)
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
+
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
