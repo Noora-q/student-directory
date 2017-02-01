6cf4ce1ba71fb2f0c917a53a17132ece27aed6b9
Noora <n.k.alqasimi@gmail.com>
2017-02-01 22:20:22 +0000

    Save data to file

--- directory.rb
+++ directory.rb
@@ -27,7 +27,6 @@ end
 
 
 def interactive_menu
-  students = []
   loop do
     # 1. print the menu and ask the user what to do
     print_menu
@@ -40,6 +39,7 @@ def print_menu
   # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -50,6 +50,18 @@ def show_students
   print_footer
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+  puts "Saved!"
+end
 
 def process(selection)
   case selection
@@ -57,6 +69,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else
